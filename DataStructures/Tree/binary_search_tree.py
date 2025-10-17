9e8febf5b87e1a6f8e1d8d6712f433fa5fcb6cb6
Juliana-_Rod-1234 <js.rodriguezm1234@uniandes.edu.co>
2025-10-17 07:47:54 -0500

    arreglo size

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,27 +1,29 @@
+from DataStructures.Tree import bst_node as n
 def new_map():
     return {"root": None}
+
 def size(my_bst):
-    def size_tree(node):
-        if node is None:
-            return 0
-        else:
-            return 1 + size_tree(node['left']) + size_tree(node['right'])
-def insert_node(root, key, value):
-    if root is None:
-        return {'key': key, 'value':value, 'left': None, 'right': None}
+    return size_tree(my_bst["root"])
+    
+def size_tree(node):
+    if node is None:
+        return 0
     else:
-        if key == root['key']:
-            root['value'] = value
-        elif key < root['key']:
-            root['left'] = insert_node(root['left'], key, value)
-        else:
-            root['right'] = insert_node(root['right'], key, value)
-    return root
+        return 1 + size_tree(node['left']) + size_tree(node['right'])
 
 def put(my_bst, key, value):
-    my_bst = insert_node(my_bst, key, value)
-    return my_bst
+    return insert_node(my_bst["root"], key, value)
 
+def insert_node(root, key, value):
+    if root is None:
+        root["root"] = n.new_node(key, value)
+    elif key == root['key']:
+        root['value'] = value
+        return root
+    elif key < root['key']:
+        return insert_node(root['left'], key, value)
+    else:
+        return insert_node(root['right'], key, value)
 
 def get(bst, key):
     valor = None
