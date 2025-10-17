ed95940ba48ac1e61f973940d0cbf0d66334a18c
jlozadab <j.lozadab@uniandes.edu.co>
2025-10-17 07:25:51 -0500

    size mejor

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,11 +1,9 @@
 def new_map():
     return {"root": None}
-def size(m):
-    def size_helper(node):
-        if node is None:
-            return 0
-        return 1 + size_helper(node["left"]) + size_helper(node["right"])
-    return size_helper(m["root"])
+def size(node):
+    if node is None:
+        return 0
+    return 1 + size(node['left']) + size(node['right'])
 def insert_node(root, key, value):
     if root is None:
         return {'key': key, 'value':value, 'left': None, 'right': None}
