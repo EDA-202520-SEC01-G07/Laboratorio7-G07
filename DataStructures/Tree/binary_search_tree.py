d97ed1074f1f3fbc23cd531b9e3e4b38e5b9021f
jlozadab <j.lozadab@uniandes.edu.co>
2025-10-17 07:22:09 -0500

    size,new map

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,2 +1,8 @@
 def new_map():
-    return {"root": None, "size": 0}
+    return {"root": None}
+def size(m):
+    def size_helper(node):
+        if node is None:
+            return 0
+        return 1 + size_helper(node["left"]) + size_helper(node["right"])
+    return size_helper(m["root"])
